0e1f656947d4dc5a11d760f7e51e83f9f9246992
5237-mests <msfnmulgeta@gmail.com>
2024-10-04 16:58:20 +0300

    prisma added.

---
+++ app/people/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+// get people from db
+const getPeople = async () => {
+    const people = await fetch("/api/people");
+    return people.json();
+}
+export default async function Page() {
+    return <div className="mt-10 mb-20" >
+        <h1 className="bg-gray-100 w-full p-4 text-2xl font-bold mb-4 fixed top-0"  >People</h1> 
+        {/* {people} */}
+        <button onClick={getPeople} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-10">
+            Get user
+        </button>
+    </div>;
+}
