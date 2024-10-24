5359bbccd080f3a78244e2738ee89392a51324f8
yabiball <yabibalhunie@gmail.com>
2024-10-24 13:24:43 +0300

    integreat product page to the api

--- components/dashboard/product/addProduct.tsx
+++ components/dashboard/product/addProduct.tsx
@@ -18,18 +18,17 @@ interface Product {
 interface AddProductProps {
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  addProduct: (product: Product) => void;
   categories: Category[];
-  handleProductSubmit: () => void;
+  fetchProducts: () => void;
 }
 
 const AddProduct: React.FC<AddProductProps> = ({
   isModalOpen,
   setIsModalOpen,
-  addProduct,
   categories,
-  handleProductSubmit,
+  fetchProducts,
 }) => {
+  const [submitStatus, setSubmitStatus] = useState<string>("Add Product");
   const [newProduct, setNewProduct] = useState<Product>({
     id: null,
     name: "",
@@ -66,34 +65,46 @@ const AddProduct: React.FC<AddProductProps> = ({
     }));
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleProductSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setSubmitStatus("Submitting...");
+    const formData = new FormData();
+    Object.keys(newProduct).forEach((key) => {
+      const value = newProduct[key as keyof typeof newProduct];
+      if (key !== "file" && value !== null) {
+        formData.append(key, String(value));
+      }
+    });
 
-    // Basic validation
-    if (newProduct.price <= 0) {
-      alert("Price must be a positive number.");
-      return;
+    if (newProduct.file) {
+      formData.append("file", newProduct.file);
     }
 
-    const formData = new FormData();
-    Object.entries(newProduct).forEach(([key, value]) => {
-      formData.append(key, value);
+    const url = newProduct.id
+      ? `/api/product/${newProduct.id}`
+      : "/api/product";
+    const method = newProduct.id ? "PUT" : "POST";
+    const res = await fetch(url, {
+      method,
+      body: formData,
     });
-    try {
-      await addProduct(newProduct); // Update this if your addProduct needs FormData
-      setNewProduct({
-        id: null,
-        name: "",
-        description: "",
-        price: 0,
-        available: false,
-        category_id: "",
-        file: null,
-      }); // Reset the form
-      handleProductSubmit();
-      setIsModalOpen(false);
-    } catch (error) {
-      console.error("Failed to add product:", error);
+
+    if (res.ok) {
+      setSubmitStatus("Product Added Successfully");
+      setTimeout(() => {
+        setIsModalOpen(false);
+        setSubmitStatus("Add Product");
+        setNewProduct({
+          id: null,
+          name: "",
+          description: "",
+          price: 0,
+          available: false,
+          category_id: "",
+          file: null,
+        });
+        fetchProducts();
+      }, 2000); // Wait for 2 seconds
     }
   };
 
@@ -139,7 +150,7 @@ const AddProduct: React.FC<AddProductProps> = ({
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
-              <form className="p-4 md:p-5" onSubmit={handleSubmit}>
+              <form className="p-4 md:p-5" onSubmit={handleProductSubmit}>
                 <div className="grid gap-4 mb-4 grid-cols-2">
                   <div className="col-span-2">
                     <label
@@ -262,12 +273,37 @@ const AddProduct: React.FC<AddProductProps> = ({
                     </div>
                   </div>
                 </div>
-             
-                <button
-                  type="submit"
+
+                {/* <button
+                  // type="submit"
                   className="bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-500 transition duration-200 ease-in-out"
                 >
-                  Add Product
+                  {submitStatus}
+                </button> */}
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                >
+                  {submitStatus === "Submitting..." && (
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 me-3 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  )}
+                  {submitStatus}
                 </button>
               </form>
             </div>
